f56ea182d21098d1f6c6f5fd3cb612d890d4c86b
Carla <carlachamandi@gmail.com>
2025-10-15 09:49:01 -0500

    riderController edit

--- Backend/veloMTL/src/main/java/com/veloMTL/veloMTL/Controller/RiderController.java
+++ Backend/veloMTL/src/main/java/com/veloMTL/veloMTL/Controller/RiderController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RiderController {
 
-    private final RiderService riderService;
+    private final RiderService riderService; // change to facade obj
 
     public RiderController(RiderService riderService) {
         this.riderService = riderService;
