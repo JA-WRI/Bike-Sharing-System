f6f68d6e6d667f1eb3dd77698d9a5ecfe0a5b979
Carla <carlachamandi@gmail.com>
2025-10-19 17:47:55 -0500

    pulled from jaden

--- Backend/veloMTL/src/main/java/com/veloMTL/veloMTL/Repository/OperatorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.veloMTL.veloMTL.Repository;
-
-import com.veloMTL.veloMTL.Model.Users.Operator;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface OperatorRepository extends MongoRepository<Operator, String> {
-}
