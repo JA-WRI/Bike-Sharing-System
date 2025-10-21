6c82070933a8fbf53de8f5871f12607ccac8327b
hj235 <hongjing235@gmail.com>
2025-10-20 21:49:54 -0400

    Update RiderCommandFactory

--- Backend/veloMTL/src/main/java/com/veloMTL/veloMTL/Patterns/Factory/RiderCommandFactory.java
+++ Backend/veloMTL/src/main/java/com/veloMTL/veloMTL/Patterns/Factory/RiderCommandFactory.java
@@ -2,37 +2,36 @@ package com.veloMTL.veloMTL.Patterns.Factory;
 
 import com.veloMTL.veloMTL.DTO.Helper.CommandDTO;
 import com.veloMTL.veloMTL.Patterns.Command.Command;
-import com.veloMTL.veloMTL.Patterns.Command.OperatorCommands.*;
+import com.veloMTL.veloMTL.Patterns.Command.RiderCommands.*;
+import com.veloMTL.veloMTL.Service.BMSCore.BikeService;
 import com.veloMTL.veloMTL.Service.BMSCore.DockService;
+import com.veloMTL.veloMTL.Service.BMSCore.StationService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RiderCommandFactory extends CommandFactory{
     private final DockService dockService;
+    private final StationService stationService;
+    private final BikeService bikeService;
 
 
-    public RiderCommandFactory(DockService dockService) {
+    public RiderCommandFactory(DockService dockService, StationService stationService, BikeService bikeService){
         this.dockService = dockService;
+        this.stationService = stationService;
+        this.bikeService = bikeService;
     }
 
 
     @Override
     public Command<?> createCommand(CommandDTO commandDTO) {
-        return null;
-//        return switch (commandDTO.getCommand()) {
-//            //mark dock out of service
-//            case "MDOS" -> new MarkDockOutOfService(dockService, commandDTO.getUserId(), commandDTO.getObjectId());
-//            //mark station out of service
-//            case "MSOS" -> new MarkStationOutOfService(stationService, commandDTO.getUserId(), commandDTO.getObjectId());
-//            //restore dock
-//            case "RD" -> new RestoreDock(dockService, commandDTO.getUserId(), commandDTO.getObjectId());
-//            //restore station
-//            case "RS" -> new RestoreStation(stationService, commandDTO.getUserId(), commandDTO.getObjectId());
-//            //unlock bike
-//            case "UB" -> new OperatorUnlockBike(bikeService, commandDTO.getUserId(), commandDTO.getObjectId());
-//            //lock bike
-//            case "LB" -> new OperatorLockBike(bikeService, commandDTO.getUserId(), commandDTO.getObjectId(), commandDTO.getDockId());
-//            default -> throw new IllegalArgumentException("Unknown action: " + commandDTO.getCommand());
-//        };
+        return switch (commandDTO.getCommand()) {
+            //unlock bike
+            case "UB" -> new RiderUnlockBike(bikeService, commandDTO.getUserId(), commandDTO.getObjectId());
+            //lock bike
+            case "LB" -> new RiderLockBike(bikeService, commandDTO.getUserId(), commandDTO.getObjectId(), commandDTO.getDockId());
+            //reserve bike
+            case "RB" -> new RiderReserveBike(bikeService, commandDTO.getUserId(), commandDTO.getObjectId());
+            default -> throw new IllegalArgumentException("Unknown rider action: " + commandDTO.getCommand());
+        };
     }
 }
