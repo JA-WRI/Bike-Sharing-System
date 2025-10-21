29241ba812ceda994c046bf877c25f04fbad5dd1
hj235 <hongjing235@gmail.com>
2025-10-20 21:44:21 -0400

    Create Rider Commands

--- Backend/veloMTL/src/main/java/com/veloMTL/veloMTL/Patterns/Factory/RiderCommandFactory.java
+++ Backend/veloMTL/src/main/java/com/veloMTL/veloMTL/Patterns/Factory/RiderCommandFactory.java
@@ -2,6 +2,7 @@ package com.veloMTL.veloMTL.Patterns.Factory;
 
 import com.veloMTL.veloMTL.DTO.Helper.CommandDTO;
 import com.veloMTL.veloMTL.Patterns.Command.Command;
+import com.veloMTL.veloMTL.Patterns.Command.OperatorCommands.*;
 import com.veloMTL.veloMTL.Service.BMSCore.DockService;
 import org.springframework.stereotype.Service;
 
@@ -18,5 +19,20 @@ public class RiderCommandFactory extends CommandFactory{
     @Override
     public Command<?> createCommand(CommandDTO commandDTO) {
         return null;
+//        return switch (commandDTO.getCommand()) {
+//            //mark dock out of service
+//            case "MDOS" -> new MarkDockOutOfService(dockService, commandDTO.getUserId(), commandDTO.getObjectId());
+//            //mark station out of service
+//            case "MSOS" -> new MarkStationOutOfService(stationService, commandDTO.getUserId(), commandDTO.getObjectId());
+//            //restore dock
+//            case "RD" -> new RestoreDock(dockService, commandDTO.getUserId(), commandDTO.getObjectId());
+//            //restore station
+//            case "RS" -> new RestoreStation(stationService, commandDTO.getUserId(), commandDTO.getObjectId());
+//            //unlock bike
+//            case "UB" -> new OperatorUnlockBike(bikeService, commandDTO.getUserId(), commandDTO.getObjectId());
+//            //lock bike
+//            case "LB" -> new OperatorLockBike(bikeService, commandDTO.getUserId(), commandDTO.getObjectId(), commandDTO.getDockId());
+//            default -> throw new IllegalArgumentException("Unknown action: " + commandDTO.getCommand());
+//        };
     }
 }
