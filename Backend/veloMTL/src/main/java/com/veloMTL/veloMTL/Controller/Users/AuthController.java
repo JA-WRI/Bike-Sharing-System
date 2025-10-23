a69b9ea4664bd5e8bcfb298bc78843d21f286d8c
Carla <carlachamandi@gmail.com>
2025-10-23 17:36:36 -0500

    Fixed the login (regular & google), added the operators login

--- Backend/veloMTL/src/main/java/com/veloMTL/veloMTL/Controller/Users/AuthController.java
+++ Backend/veloMTL/src/main/java/com/veloMTL/veloMTL/Controller/Users/AuthController.java
@@ -1,32 +1,56 @@
 package com.veloMTL.veloMTL.Controller.Users;
 
+import com.veloMTL.veloMTL.DTO.Users.LoginDTO;
 import com.veloMTL.veloMTL.DTO.Users.RegistrationDTO;
 import com.veloMTL.veloMTL.Model.Users.Rider;
-import com.veloMTL.veloMTL.Service.Auth.RegistrationService;
+import com.veloMTL.veloMTL.Security.JwtService;
+import com.veloMTL.veloMTL.Service.Auth.AuthService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    private final RegistrationService registrationService;
+    private final AuthService authService;
+    private final JwtService jwtService;
 
-    public AuthController(RegistrationService registrationService){
-        this.registrationService = registrationService;
+    public AuthController(AuthService authService, JwtService jwtService){
+        this.authService = authService;
+        this.jwtService = jwtService;
     }
 
     @PostMapping("/register")
     public ResponseEntity<?> registerRider (@RequestBody RegistrationDTO registrationDTO){
         try {
-            Rider rider = registrationService.registerRider(registrationDTO);
+            Rider rider = authService.registerRider(registrationDTO);
             return ResponseEntity.ok("Rider registered successfully with email: " + rider.getEmail());
         } catch (Exception e){
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
 
+    public ResponseEntity<?> googleLoginRider (@RequestParam String token){
+
+        String email = jwtService.extractEmail(token);
+        return ResponseEntity.ok("yooooo you are logged in my dude :p");
+    }
+
+    @GetMapping("/dashboard")
+    public String loginSuccess() {
+        return " Google login successful! Welcome to your dashboard.";
+    }
+    @PostMapping("/rider/login")
+    public ResponseEntity<?> riderLogin(@RequestBody LoginDTO loginDTO){
+        String token = authService.loginRider(loginDTO);
+        return ResponseEntity.ok(Map.of("token",token));
+    }
+
+    @PostMapping("/operator/login")
+    public ResponseEntity<?> operatorLogin(@RequestBody LoginDTO loginDTO){
+        String token = authService.loginOperator(loginDTO);
+        return ResponseEntity.ok(Map.of("token",token));
+    }
 }
