29aea3550200e009aaf74493a26189e2b7f763b9
Jaden <jadenwrightmaurais@gmail.com>
2025-10-24 00:41:06 -0400

    Fixed command for locking a bike and fixed authentication for rider

--- Backend/veloMTL/src/main/java/com/veloMTL/veloMTL/Patterns/State/Bikes/OnTripBikeState.java
+++ Backend/veloMTL/src/main/java/com/veloMTL/veloMTL/Patterns/State/Bikes/OnTripBikeState.java
@@ -2,6 +2,7 @@ package com.veloMTL.veloMTL.Patterns.State.Bikes;
 
 import com.veloMTL.veloMTL.Model.BMSCore.Bike;
 import com.veloMTL.veloMTL.Model.BMSCore.Dock;
+import com.veloMTL.veloMTL.Model.Enums.BikeStatus;
 import com.veloMTL.veloMTL.Model.Enums.StateChangeStatus;
 import com.veloMTL.veloMTL.Model.Enums.UserStatus;
 import com.veloMTL.veloMTL.untils.Responses.StateChangeResponse;
@@ -17,8 +18,8 @@ public class OnTripBikeState implements BikeState{
     @Override
     public StateChangeResponse lockBike(Bike bike, Dock dock) {
 
-        //***add logic here***
-
+        bike.setBikeStatus(BikeStatus.AVAILABLE);
+        bike.setState(new AvailableBikeState());
         return new StateChangeResponse(StateChangeStatus.SUCCESS, "Bike has been successfully put back");
     }
 
