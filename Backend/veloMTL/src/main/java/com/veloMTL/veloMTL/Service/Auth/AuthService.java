0a94a92ce30535c47edeceaa715c3200d3ed6264
Carla <carlachamandi@gmail.com>
2025-11-06 16:48:13 -0500

    fixed bugg

--- Backend/veloMTL/src/main/java/com/veloMTL/veloMTL/Service/Auth/AuthService.java
+++ Backend/veloMTL/src/main/java/com/veloMTL/veloMTL/Service/Auth/AuthService.java
@@ -16,37 +16,37 @@ import java.util.Optional;
 
 @Service
 public class AuthService {
-    private final RiderRepository riderRepository;
-    private final OperatorRepository operatorRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final JwtService jwtService;
+        private final RiderRepository riderRepository;
+        private final OperatorRepository operatorRepository;
+        private final PasswordEncoder passwordEncoder;
+        private final JwtService jwtService;
 
-    public AuthService(RiderRepository riderRepository, OperatorRepository operatorRepository, PasswordEncoder passwordEncoder, JwtService jwtService){
-        this.riderRepository = riderRepository;
-        this.operatorRepository = operatorRepository;
-        this.passwordEncoder = passwordEncoder;
-        this.jwtService = jwtService;
-    }
+        public AuthService(RiderRepository riderRepository, OperatorRepository operatorRepository, PasswordEncoder passwordEncoder, JwtService jwtService){
+            this.riderRepository = riderRepository;
+            this.operatorRepository = operatorRepository;
+            this.passwordEncoder = passwordEncoder;
+            this.jwtService = jwtService;
+        }
 
     public LoginResponseDTO registerRider (RegistrationDTO registrationDTO){
-    if(riderRepository.existsByEmail(registrationDTO.getEmail())){
-        throw  new RuntimeException("Email already used");
-    }
-     String encodedPassword = passwordEncoder.encode(registrationDTO.getPassword());
-     Rider rider = new Rider(
-            registrationDTO.getName(),
-            registrationDTO.getEmail(),
-            encodedPassword
-    );
-    riderRepository.save(rider);
-    String token = jwtService.generateToken(rider.getEmail(), rider.getRole(), rider.getPermissions());
+        if(riderRepository.existsByEmail(registrationDTO.getEmail())){
+            throw  new RuntimeException("Email already used");
+        }
+        String encodedPassword = passwordEncoder.encode(registrationDTO.getPassword());
+        Rider rider = new Rider(
+                registrationDTO.getName(),
+                registrationDTO.getEmail(),
+                encodedPassword
+        );
+        riderRepository.save(rider);
+        String token = jwtService.generateToken(rider.getEmail(), rider.getRole(), rider.getPermissions());
 
-    return new LoginResponseDTO(
-            token,
-            rider.getName(),
-            rider.getEmail(),
-            rider.getRole()
-    );
+        return new LoginResponseDTO(
+                token,
+                rider.getName(),
+                rider.getEmail(),
+                rider.getRole()
+        );
     }
 
     public LoginResponseDTO registerGoogleUser(String name, String email) {
@@ -65,23 +65,22 @@ public class AuthService {
 
     }
 
-public LoginResponseDTO loginRider(LoginDTO loginDTO){
+    public LoginResponseDTO loginRider(LoginDTO loginDTO){
         Rider rider = riderRepository.findByEmail(loginDTO.getEmail())
                 .orElseThrow(() -> new RuntimeException("wrong email"));
 
-    if(!passwordEncoder.matches(loginDTO.getPassword(), rider.getPassword())) {
-        throw new RuntimeException("Invalid password");
-    }
-    String token = jwtService.generateToken(rider.getEmail(), rider.getRole(), rider.getPermissions());
+        if(!passwordEncoder.matches(loginDTO.getPassword(), rider.getPassword())) {
+            throw new RuntimeException("Invalid password");
+        }
+        String token = jwtService.generateToken(rider.getEmail(), rider.getRole(), rider.getPermissions());
 
-    return new LoginResponseDTO(
-            token,
-            rider.getId(),
-            rider.getName(),
-            rider.getEmail(),
-            rider.getRole()
-    );
-}
+        return new LoginResponseDTO(
+                token,
+                rider.getName(),
+                rider.getEmail(),
+                rider.getRole()
+        );
+    }
 
     public LoginResponseDTO loginOperator(LoginDTO loginDTO) {
         Operator operator = (Operator) operatorRepository.findByEmail(loginDTO.getEmail())
@@ -94,10 +93,10 @@ public LoginResponseDTO loginRider(LoginDTO loginDTO){
 
         return new LoginResponseDTO(
                 token,
-                operator.getId(),
                 operator.getName(),
                 operator.getEmail(),
                 operator.getRole()
         );
     }
+
 }
