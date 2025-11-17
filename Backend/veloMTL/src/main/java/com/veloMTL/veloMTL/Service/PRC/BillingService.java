9bc32ea8ddf607d04a8c455aa79d7911eec3a279
Carla <carlachamandi@gmail.com>
2025-11-17 11:56:10 -0500

    removed print statements

--- Backend/veloMTL/src/main/java/com/veloMTL/veloMTL/Service/PRC/BillingService.java
+++ Backend/veloMTL/src/main/java/com/veloMTL/veloMTL/Service/PRC/BillingService.java
@@ -72,9 +72,6 @@ public class BillingService {
 
             int percentageCapacity = Math.round((station.getOccupancy()-1/station.getCapacity())*10);
 
-            System.out.println("\n\n\n\n\n\n percentageCapacity " + percentageCapacity);
-
-
             boolean isEBike = trip.getBike().getBikeType().equalsIgnoreCase("electric");
             long tripDuration = getTripDurationInMinutes(trip);
             double ratePerMinute = plan.getRatebyMinute();
@@ -82,14 +79,12 @@ public class BillingService {
             if (user instanceof Operator) {
                 tripCost = plan.calculateTripCost(tripDuration, isEBike, flexDollars,riderRepository,operatorRepository, user.getId(),percentageCapacity );
                 tripCost = (double) Math.round((tripCost*(1-0.05))*100)/100.0;
-                //tripCost = Math.round(((tripCost*(1-0.05))*100.00)/100.00);
-                System.out.println("\n\n\n\n\n\n\n tripCost "+ tripCost);
+
                 Operator op = operatorRepository.findById(user.getId()).orElseThrow(()-> new RuntimeException("No operator found with id"));
                 operatorRepository.save(op);
             } else {
                 tripCost = plan.calculateTripCost(tripDuration, isEBike, flexDollars, riderRepository,operatorRepository, user.getId(),percentageCapacity );
-                //tripCost = Math.round((tripCost*100.00)/100.00);
-                System.out.println("\n\n\n\n\n\n\n tripCost "+ tripCost);
+
                 Rider ri = riderRepository.findById(user.getId()).orElseThrow(()-> new RuntimeException("No rider found with id"));
                 riderRepository.save(ri);
             }
